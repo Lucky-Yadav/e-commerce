e90945188cb696ef7bc841780f7e876827748554
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-29 21:17:01 +0530

    Update Home.jsx

--- e-commerce-1/src/components/Home.jsx
+++ e-commerce-1/src/components/Home.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import { useSelector } from "react-redux";
 
 const Home = () => {
+const token= useSelector(state.auth.token)
+
+    if (!token) {
+        
+    }
   return (
     <div>Home</div>
   )
