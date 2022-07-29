7a6c2b889c0478f9b782db463a9567f210f962cc
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-29 20:36:49 +0530

    Update action.js

---
+++ e-commerce-1/src/store/auth/action.js
@@ -0,0 +1,42 @@
+import axios from "axios";
+import { LOGIN_REQ, LOGIN_SUCCESS, LOGOUT_PAGE } from "./actiontype";
+
+export const loginregister = (payload) => {
+  return {
+    type: LOGIN_REQ,
+    payload,
+  };
+};
+
+export const sucessLoginreq = (payload) => {
+  return {
+    type: LOGIN_SUCCESS,
+    payload,
+  };
+};
+
+export const logoutpage = () => {
+  return {
+    type: LOGOUT_PAGE,
+  };
+};
+
+export const loginruser = (payload) => (dispatch) => {
+  dispatch(loginregister());
+
+  axios
+    .post(`https://reqres.in/api/login`, payload, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+    .then((response) => {
+      let pay = {
+        token: response.token,
+        username: payload.username,
+      };
+      console.log(response.data.token);
+      dispatch(sucessLoginreq(pay));
+    })
+    .catch((err) => console.log(err));
+};
