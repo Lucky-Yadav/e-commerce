f51db86501ad97012323e016b193657f25bb8679
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-29 18:34:47 +0530

    update login and navbar

---
+++ e-commerce-1/src/components/Login.jsx
@@ -0,0 +1,47 @@
+import React, { useContext } from "react";
+import { useState } from "react";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import SendIcon from "@mui/icons-material/Send";
+import { AuthContext } from "../context/AuthContext";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [pass, setpass] = useState("");
+  const { token, handleLogin, setToken } = useContext(AuthContext);
+  const handleAuth = (details) => {
+    token ? setToken(null) : handleLogin(details);
+  };
+
+  return (
+    <div>
+      <TextField
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        id="outlined-basic"
+        label="Email"
+        variant="outlined"
+      />
+      <TextField
+        value={pass}
+        onChange={(e) => setpass(e.target.value)}
+        id="outlined-basic"
+        label="pass"
+        variant="outlined"
+      />
+      <br />
+      <div className="button">
+        <br />
+        <Button
+          onClick={() => handleAuth({ email, password: pass })}
+          variant="contained"
+          endIcon={<SendIcon />}
+        >
+          {token ? "log out" : "log in"}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
