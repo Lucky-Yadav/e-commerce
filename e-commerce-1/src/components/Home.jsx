6cfbdc9b36ea7268b510836ef71acf34745cabcc
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-07-30 11:29:42 +0530

    Update Home.jsx

--- e-commerce-1/src/components/Home.jsx
+++ e-commerce-1/src/components/Home.jsx
@@ -1,7 +1,39 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 // import { useSelector } from "react-redux";
 
 const Home = () => {
+  const [products, setproducts] = useState({
+    loading: false,
+    error: false,
+    data: []
+  })
+
+  useEffect(() => {
+    setproducts((prev) => ({
+      ...prev,
+      loading: true
+    }));
+    axios({
+      method: "get",
+      url: "http://localhost:4000/products",
+    })
+      .then((res) =>
+        setproducts((prev) => ({
+          ...prev,
+          loading: false,
+          error: false,
+          data: res.data
+        }))
+      )
+      .catch((err) =>
+        setproducts((prev) => ({
+          ...prev,
+          loading: false,
+          error: true
+        }))
+      );
+  }, [])
     
   return (
     <div>Home data</div>
