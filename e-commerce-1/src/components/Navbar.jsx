324744af0673ccb4f76c64fdb4b62169f91c43c1
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-08-04 00:13:29 +0530

    Update Navbar.jsx

--- e-commerce-1/src/components/Navbar.jsx
+++ e-commerce-1/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
           ) : (
             <Link to="/login">
               <Box style={{ cursor: "pointer" }} sx={{ flexGrow: 0 }}>
-                Sign
+                Sign up
               </Box>
             </Link>
           )}
