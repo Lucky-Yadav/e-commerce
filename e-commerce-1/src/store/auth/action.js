d159245120deaa4f08b774bac08ac73a40760db0
Luckyyadav420 <97690042+Lucky-Yadav@users.noreply.github.com>
2022-08-04 00:10:39 +0530

    Update action.js

--- e-commerce-1/src/store/auth/action.js
+++ e-commerce-1/src/store/auth/action.js
@@ -25,22 +25,4 @@
     };
     };
 
-    // export const loginruser = (payload) => (dispatch) => {
-    // // dispatch(loginregister());
-
-    // axios
-    //     .post(`https://reqres.in/api/login`, payload, {
-    //     headers: {
-    //         "Content-Type": "application/json",
-    //     },
-    //     })
-    //     .then((response) => {
-    //     let pay = {
-    //         token: response.token,
-    //         username: payload.username,
-    //     };
-    //     console.log(response.data.token);
-    //     dispatch(sucessLoginreq(pay));
-    //     })
-    //     .catch((err) => console.log(err));
-    // };
+ 
